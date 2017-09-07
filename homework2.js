6f0783ebc6563a85d87f91110f924c254e0581df
Student Generic Account <ADStudent@auaad.aua.am>
2017-09-07 16:39:09 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,21 @@
+const time = 12;
+if (time < 9 || time > 22 ) {
+
+	alert("closed");
+
+	console.log("closed");
+
+}else {
+
+	alert("open");
+	console.log("open")
+
+}
+const num1 = 15;
+const num2 = 30;
+const num3 = 2;
+
+const add = num1 + num2 ;
+const division = num2 / num3; 
+console.log(add);
+console.log(division);
